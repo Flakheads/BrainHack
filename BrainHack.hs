1971d36a40700c9e6fee91453cc6ea68541dbcd4
wheatwizard <olivee3@rpi.edu>
2017-07-07 19:13:00 -0400

    Added -f

--- BrainHack.hs
+++ BrainHack.hs
@@ -17,6 +17,20 @@ split :: String -> [String]
 split [] = []
 split (x:xs) = (x:[]):(split xs)
 
+parseFile :: String -> [String]
+parseFile [] = [""]
+parseFile ('"':xs) = parseString xs '"'
+parseFile ('\'':xs) = parseString xs '\''
+parseFile (' ':xs) = "":parseFile xs
+parseFile ('\t':xs) = "":parseFile xs
+parseFile ('\n':xs) = "":parseFile xs
+parseFile (s:xs) = (\(a:xa)->(a++[s]):xa)$ parseFile xs
+
+parseString :: String -> Char -> [String]
+parseString (s:xs) a
+ | s == a = "":parseFile xs
+ | otherwise = (\(a:xa)->(a++[s]):xa)$ parseString xs a
+
 parseFlags :: [String] -> [String] -> [String]
 parseFlags [] a = a
 parseFlags (('-':'-':x):xs) a = parseFlags xs (x:a)
@@ -29,7 +43,10 @@ processInput (('"':ys):xs)
 processInput (('\'':ys):xs)
  | (last ys) == '\'' = (map (toInteger.ord) (init ys))++processInput xs
  | otherwise = error "Invalid input"
-processInput (x:xs) = (read x :: Integer):processInput xs
+processInput ([]:xs) = processInput xs
+processInput (x:xs)
+ | (elem (head x) "0123456789-") && (all (isDigit)$tail x) = (read x :: Integer):processInput xs
+ | otherwise = error "Invalid input"
 processInput [] = []
 
 helpMenu :: String
@@ -43,7 +60,8 @@ helpMenu = unlines ["",
   "   -h, --help\t\t\tPrints this menu and exits.",
   "   -m, --mini\t\t\tRun in an interpreter optimized for miniflak,",
   "   -v, --version\t\tPrints the version number of the interpreter and exits.",
-  "   -x, --cycles\t\t\tPrints the number of cycles elapsed upon termination."]
+  "   -x, --cycles\t\t\tPrints the number of cycles elapsed upon termination.",
+  "   -f, --file\t\t\tInput is read from file instead of commandline.  (Provide the filename as the last argument instead of input)"]
 
 displayInteger :: Integer -> String
 displayInteger x = show x ++ " "
@@ -60,15 +78,16 @@ main = do
              (return "")
            else
              (if ((elem "e" flags)||(elem "execute" flags)) then
-               (return (head args))
+               (return (head args)) --TODO ensure head
              else
-               (readFile (head args))
+               (readFile (head args)) --TODO ensure head
              )
            )
  let formatOutput = if (elem "a" flags)||(elem "ascii-out" flags) then (\x -> (chr.fromInteger) x:[]) else displayInteger
  let cycles = (elem "x" flags)||(elem "cycles" flags)
+ input <- if ((elem "f" flags)||(elem "file" flags)) then (do file <- readFile$ last args; return $ parseFile file) else (return$ tail args)
  putStr$ (if exiting then
            ""
           else
-           (\ (a,b) -> (concat (map formatOutput a)) ++ (if cycles then ("\nCycles: " ++ show b) else "")) (if (elem "m" flags)||(elem "mini" flags) then (miniflak source (processInput (tail args)) cycles) else (brainflak source (processInput (tail args)) cycles))
+           (\ (a,b) -> (concat (map formatOutput a)) ++ (if cycles then ("\nCycles: " ++ show b) else "")) (if (elem "m" flags)||(elem "mini" flags) then (miniflak source (processInput input) cycles) else (brainflak source (processInput input) cycles))
           )
